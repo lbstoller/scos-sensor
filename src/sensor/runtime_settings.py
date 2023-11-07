bc63fab7eeb64d779fe88d7082540ce985c10aa1
Leigh B Stoller <stoller@flux.utah.edu>
2023-11-07 15:32:55 -0700

    Minor localization changes.

--- src/sensor/runtime_settings.py
+++ src/sensor/runtime_settings.py
@@ -260,6 +260,7 @@ if AUTHENTICATION == "JWT":
     )
 else:
     REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = (
+        "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     )
